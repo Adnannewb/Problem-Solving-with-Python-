9c1f5a0166602423948b5283f53eec00e01f9c1a
Adnannewb <rahadnan6@gmail.com>
2026-09-10 16:48:29 +0600

    numpy array

---
+++ numpy_array.py
@@ -0,0 +1,11 @@
+import numpy
+
+def arrays(arr):
+    # complete this function
+    # use numpy.array
+    result=numpy.array(arr,float)
+    return numpy.flip(result)
+
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
